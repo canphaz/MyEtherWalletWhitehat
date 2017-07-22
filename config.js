fdd40bf19c3ea639d648947a4ed4841d1941aa2e
MrLuit <me@luithollander.nl>
2017-07-22 14:30:47 +0200

    Fix typo

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
+// View your statistics on https://lu1t.nl
+
 module.exports = {
-	"deviceID": null,				    // When set to false, an anonymous device id will be asigned.		
-	"debug": false,                     // Enable debug options (extra reporting)
+	"deviceID": null,					// When set to null, an anonymous device id will be asigned.	
+	"debug": false,						// Enable debug options (extra reporting)
 	"interval": 1000,					// The amount of delay between each request in ms (milliseconds).			
-	"enableHeartbeat": true, 	// Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made.
-	"autoUpdateData": true		// Please enable to make sure you always get the latest dataset.
+	"enableHeartbeat": true,		// Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made.
+	"autoUpdateData": true			// Please enable to make sure you always get the latest dataset.
 };
