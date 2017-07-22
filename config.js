d62f8ca8560cca381941b6d7cf50d0c732d5436f
MrLuit <me@luithollander.nl>
2017-07-22 13:17:21 +0200

    Oops

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	"deviceID": null,				     // When set to false, an anonymous device id will be asigned.		
+	"deviceID": null,				    // When set to false, an anonymous device id will be asigned.		
 	"debug": false,                     // Enable debug options (extra reporting)
 	"interval": 1000,					// The amount of delay between each request in ms (milliseconds).			
 	"enableHeartbeat": true, 	// Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made.
-	"autoUpdateData": false		// Please enable to make sure you always get the latest dataset.
+	"autoUpdateData": true		// Please enable to make sure you always get the latest dataset.
 };
