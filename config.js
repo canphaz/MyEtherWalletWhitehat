62cce12bcc4ab8102073e9b5a8cb0af12cb16e18
MrLuit <me@luithollander.nl>
2017-07-22 21:08:17 +0200

    I can't do anything right

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
 	"deviceID": null,					// When set to null, an anonymous device id will be asigned.	
 	"debug": false,						// Enable debug options (extra reporting)
-	"interval": 100,					// The amount of delay between each request in ms (milliseconds).			
+	"interval": 1000,					// The amount of delay between each request in ms (milliseconds).			
 	"enableHeartbeat": true,		// Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made.
 	"autoUpdateData": true,			// Please enable to make sure you always get the latest dataset.
 	"proxy": {
