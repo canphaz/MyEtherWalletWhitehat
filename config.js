fcc46cbd0c800b705cb65a1ca7424465bb3cd708
MrLuit <me@luithollander.nl>
2017-07-22 13:16:22 +0200

    Changed to null

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	"deviceID": false,				    // When set to false, an anonymous device id will be asigned.		
+	"deviceID": null,				     // When set to false, an anonymous device id will be asigned.		
 	"debug": false,                     // Enable debug options (extra reporting)
 	"interval": 1000,					// The amount of delay between each request in ms (milliseconds).			
 	"enableHeartbeat": true, 	// Please enable heartbeat to share and receive anonymous statistics about the amount of requests being made.
-	"autoUpdateData": true		// Please enable to make sure you always get the latest dataset.
+	"autoUpdateData": false		// Please enable to make sure you always get the latest dataset.
 };
